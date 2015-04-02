34cc9eaf76fad01bf569857457d5c6fabef187d7
andregarkin <andregarkin@gmail.com>
2015-04-02 16:44:03 +0300

    add sandbox jquery files.

---
+++ sb-jquery-koshevoy/script-02.js
@@ -0,0 +1,8 @@
+/** 
+ * jQuery Course by Koshevoy 2
+ * 
+ */
+
+console.log($('#celebs'));
+console.log($('#celebs tr'));
+console.log($('div.fan р span'));
---
+++ sb-jquery-koshevoy/script.js
@@ -0,0 +1,14 @@
+/** 
+ * jQuery Course by Koshevoy
+ *
+ */
+
+
+$(document).ready(function() {
+  console.log('Hello, world!');
+});
+
+$(function() { 
+  console.log('Hello, world! Again.');
+});
+
